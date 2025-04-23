9796c4f07afbcd7e417dba2ee664921bfce1da12
luisf <luisfernandod.teixeira@gmail.com>
2025-04-23 11:15:14 -0300

    adiciona componente de filtro com formulário e estilos

--- src/app/angular-material/angular-material.module.ts
+++ src/app/angular-material/angular-material.module.ts
@@ -1,15 +1,37 @@
-import { NgModule } from "@angular/core";
-import {MatListModule} from '@angular/material/list';
-import {MatDividerModule} from '@angular/material/divider'; 
-
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDividerModule } from '@angular/material/divider';
+import {provideNativeDateAdapter} from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 @NgModule({
-imports: [
+  imports: [
     MatListModule,
     MatDividerModule,
-],
-exports: [
+    MatInputModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatSelectModule, 
+    FormsModule,
+    MatButtonModule, 
+    MatIconModule
+  ],
+  providers: [provideNativeDateAdapter()],
+  exports: [
     MatListModule,
     MatDividerModule,
-],
+    MatInputModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatSelectModule, 
+    FormsModule,
+    MatButtonModule, 
+    MatIconModule
+  ],
 })
 export class AngularMaterialModule {}
