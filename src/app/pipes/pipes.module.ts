be83520467f299a662ae589b94910da459194f69
luisf <luisfernandod.teixeira@gmail.com>
2025-04-24 11:58:17 -0300

    feat: adiciona pipe para exibir " - " caso o valor seja vazio
    Adiciona o pipe `dashIfEmpty` para exibir um " - " caso o valor seja nulo ou vazio.
    Este pipe foi implementado para melhorar a exibição de informações nos detalhes do usuário,
    garantindo que campos vazios sejam representados de forma clara na interface. O pipe foi
    aplicado aos campos Nome, Email, Função e Data de cadastro no componente `UserDetails`.

--- src/app/pipes/pipes.module.ts
+++ src/app/pipes/pipes.module.ts
@@ -2,18 +2,21 @@ import { NgModule } from "@angular/core";
 import { PhonePipe } from './phone.pipe';
 import { AdressPipe } from './address.pipe';
 import { StatusPipe } from './status.pipe';
+import { DashIfEmptyPipe } from './dash-if-empty.pipe';
 
 @NgModule({
     declarations: [
     PhonePipe,
     AdressPipe,
     StatusPipe,
+    DashIfEmptyPipe,
     
   ],
     exports: [
         PhonePipe,
         AdressPipe,
-        StatusPipe
+        StatusPipe,
+        DashIfEmptyPipe,
     ],
 })
 export class PipesModule { }
